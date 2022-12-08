a921e63a396e1db9efc9aac39c81e9ae7d64c863
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-08 10:52:09 +0300

    Added components in app module and router selector tag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ZooComponent } from './zoo/zoo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: ZooComponent},
+  {path: 'info',component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ZooComponent } from './zoo/zoo.component';
 import { ZooAnimalComponent } from './zoo-animal/zoo-animal.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZooComponent,
-    ZooAnimalComponent
+    ZooAnimalComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
