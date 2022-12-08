d9964ab8c40bb87264dc1c3c0cc730cc7a931847
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-08 18:44:30 +0300

    Added likedAnimals route and in app.html added nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { LikedPageComponent } from './liked-page/liked-page.component';
 import { ZooComponent } from './zoo/zoo.component';
 
 const routes: Routes = [
   {path: '',component: ZooComponent},
   {path: 'info',component: AboutComponent},
+  {path: 'likedAnimals', component:LikedPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ZooComponent } from './zoo/zoo.component';
 import { ZooAnimalComponent } from './zoo-animal/zoo-animal.component';
 import { AboutComponent } from './about/about.component';
+import { LikedPageComponent } from './liked-page/liked-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZooComponent,
     ZooAnimalComponent,
-    AboutComponent
+    AboutComponent,
+    LikedPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
