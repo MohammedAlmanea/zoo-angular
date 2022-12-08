149826b50e9368aa36c0b9e1aff9ccea812dddb0
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-08 10:49:55 +0300

    Created zoo component

--- src/app/zoo/zoo.component.ts
+++ src/app/zoo/zoo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { animal } from 'src/types/animal';
+import { Animal } from 'src/types/animal';
 
 @Component({
   selector: 'app-zoo',
@@ -7,7 +7,7 @@ import { animal } from 'src/types/animal';
   styleUrls: ['./zoo.component.css']
 })
 export class ZooComponent implements OnInit {
-  animalList: animal[] = [];
+  animalList: Animal[] = [];
 
   constructor() { }
 
@@ -16,19 +16,26 @@ export class ZooComponent implements OnInit {
       {
         id: 1,
         name: 'Panda',
-        fed: true
+        fed: true,
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
       },
       {
         id: 2,
         name: 'Elephant',
-        fed: true
+        fed: true,
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
       },
       {
         id: 3,
         name: 'Lion',
-        fed: false
+        fed: false,
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
       }
     ]
+  }
+
+  onLike(animal:Animal): void {
+    alert(`I like ${animal.name}`);
   }
 
 }
