c8bbece7910df01694cf923b900325db21d746d5
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-10 20:23:25 +0300

    Refactor animal-list service and Created add animal method

--- src/app/services/animal-list.service.ts
+++ src/app/services/animal-list.service.ts
@@ -2,32 +2,38 @@ import { Injectable } from '@angular/core';
 import { Animal } from 'src/types/animal';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimalListService {
+  constructor() {}
+  animalsList: Animal[] = [
+    {
+      id: 1,
+      name: 'Panda',
+      fed: true,
+      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.',
+    },
+    {
+      id: 2,
+      name: 'Elephant',
+      fed: true,
+      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.',
+    },
+    {
+      id: 3,
+      name: 'Lion',
+      fed: false,
+      info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.',
+    },
+  ];
 
-  constructor() { }
 
   getAnimals(): Animal[] {
-    return  [
-      {
-        id: 1,
-        name: 'Panda',
-        fed: true,
-        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
-      },
-      {
-        id: 2,
-        name: 'Elephant',
-        fed: true,
-        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
-      },
-      {
-        id: 3,
-        name: 'Lion',
-        fed: false,
-        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis pellentesque nisl, et blandit magna pellentesque eu. Duis sed eros dapibus, molestie magna id, rhoncus massa. Duis nec elementum libero. Nullam auctor, urna tempor luctus vestibulum, nulla ante rhoncus risus, eu pretium purus dolor eu risus. Fusce eu augue justo.'
-      }
-    ]
+    return this.animalsList
+  }
+
+  addAnimal(animal: Animal) {
+    this.animalsList.unshift(animal);
+    alert('Animal Added!!');
   }
 }
