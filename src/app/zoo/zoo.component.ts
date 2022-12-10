5c4da0b02f871cf882cf22b6bb6602b540d48fef
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-10 20:28:15 +0300

    Added create-animal

--- src/app/zoo/zoo.component.ts
+++ src/app/zoo/zoo.component.ts
@@ -5,16 +5,23 @@ import { AnimalListService } from '../services/animal-list.service';
 @Component({
   selector: 'app-zoo',
   templateUrl: './zoo.component.html',
-  styleUrls: ['./zoo.component.css']
+  styleUrls: ['./zoo.component.css'],
 })
 export class ZooComponent implements OnInit {
   animalList: Animal[] = [];
 
-  constructor(
-    private animalListService : AnimalListService
-  ) {}
+  constructor(private animalListService: AnimalListService) {}
 
   ngOnInit(): void {
     this.animalList = this.animalListService.getAnimals();
   }
+
+  addAnimal(animal: Animal) {
+    this.animalListService.addAnimal(animal);
+  }
+
+  // addAnimal(animal: Animal) {
+  //   this.animalList.unshift(animal);
+  //   alert('Animal Added!');
+  // }
 }
